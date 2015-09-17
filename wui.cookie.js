992d2a2d735bdfab62a9bf5111c89d887955728c
wusfen <404315887@qq.com>
2015-09-17 10:11:08 +0800

    ...

--- wui.cookie.js
+++ wui.cookie.js
@@ -15,49 +15,49 @@ wui.cookie('name')           //get
 */
 + function(wui) {
 
-    function cookie(name, value, options) {
-        options = options || {};
+	function cookie(name, value, options) {
+		options = options || {};
 
-        /*delete: cookie(name, null)*/
-        if (value === null) options.expires = -1;
+		/*delete: cookie(name, null)*/
+		if (value === null) options.expires = -1;
 
-        /*delete all: cookie(null)*/
-        if (name === null) {
-            for (var i in cookie()) {
-                cookie(i, null, options);
-            }
-        }
+		/*delete all: cookie(null)*/
+		if (name === null) {
+			for (var i in cookie()) {
+				cookie(i, null, options);
+			}
+		}
 
-        /*set: cookie(name, value, {})*/
-        if (value !== undefined) {
-            if (options.expires) {
-                var date = new Date;
-                date.setTime(date.getTime() + options.expires);
-            }
-            document.cookie = [name + '=' + escape(value),
-                options.expires ? ';expires=' + date.toUTCString() : '',
-                options.path ? ';path=' + options.path : '',
-                options.domain ? ';domain=' + options.domain : '',
-                options.secure ? ';secure' : ''
-            ].join('');
-        }
+		/*set: cookie(name, value, {})*/
+		if (value !== undefined) {
+			if (options.expires) {
+				var date = new Date;
+				date.setTime(date.getTime() + options.expires);
+			}
+			document.cookie = [name + '=' + escape(value),
+				options.expires ? ';expires=' + date.toUTCString() : '',
+				options.path ? ';path=' + options.path : '',
+				options.domain ? ';domain=' + options.domain : '',
+				options.secure ? ';secure' : ''
+			].join('');
+		}
 
-        /*get: cookie('name'), cookie(), cookie().name, cookie()['name']*/
-        var cookies = {},
-            nvs = document.cookie ? document.cookie.split('; ') : [];
-        for (var i in nvs) {
-            var nv = nvs[i].split('='),
-                n = nv[0],
-                v = nv[1];
-            cookies[n] = unescape(v);
-        }
+		/*get: cookie('name'), cookie(), cookie().name, cookie()['name']*/
+		var cookies = {},
+			nvs = document.cookie ? document.cookie.split('; ') : [];
+		for (var i in nvs) {
+			var nv = nvs[i].split('='),
+				n = nv[0],
+				v = nv[1];
+			cookies[n] = unescape(v);
+		}
 
-        /*log*/
-        // console.log(cookies, document.cookie);
+		/*log*/
+		// console.log(cookies, document.cookie);
 
-        /*return: value=cookie('name'), obj=cookies()*/
-        return name ? cookies[name] : cookies;
-    }
+		/*return: value=cookie('name'), obj=cookies()*/
+		return name ? cookies[name] : cookies;
+	}
 
-    wui.cookie = cookie;
+	wui.cookie = cookie;
 }(this.wui || (wui = {}));
